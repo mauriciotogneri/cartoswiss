261a054b4f65969b6cf3041c44f66ea9003c3dde
Mauricio Togneri <mauricio.togneri@gmail.com>
2022-08-28 23:21:56 +0200

    Improvements

--- frontend/README.md
+++ frontend/README.md
@@ -1,2 +1,20 @@
 https://pub.dev/packages/geoflutterfire
 https://firebase.google.com/docs/firestore/solutions/geoqueries
+
+# Places
+restaurant, bar, pub, cafe, ice cream
+toilets, drinking_water, water_point
+
+car parking, bike parking, gas station, car wash, charging_station
+atm, bank, bureau_de_change
+kiosk, supermarket, bakery, vending_machine
+bbq, picnic_table
+
+pharmacy, veterinary
+cinema, theatre
+post_office
+dog_toilet, dog_park
+photo_booth
+
+https://wiki.openstreetmap.org/wiki/Key:amenity
+https://wiki.openstreetmap.org/wiki/Key:leisure
--- frontend/lib/screens/map_screen.dart
+++ frontend/lib/screens/map_screen.dart
@@ -6,35 +6,24 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 class MapScreen extends StatelessWidget {
   final MapState state;
 
-  const MapScreen(this.state);
+  const MapScreen._(this.state);
 
-  factory MapScreen.instance() => MapScreen(MapState());
+  factory MapScreen.instance() => MapScreen._(MapState());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Content(state),
-    );
-  }
-}
-
-class Content extends StatelessWidget {
-  final MapState state;
-
-  const Content(this.state);
-
-  @override
-  Widget build(BuildContext context) {
-    return StateProvider<MapState>(
-      state: state,
-      builder: (context, state) => SizedBox(
-        width: state.mapReady ? double.infinity : 1,
-        height: state.mapReady ? double.infinity : 1,
-        child: GoogleMap(
-          mapType: MapType.normal,
-          markers: state.markers,
-          initialCameraPosition: state.initialPosition(),
-          onMapCreated: state.onMapCreated,
+      body: StateProvider<MapState>(
+        state: state,
+        builder: (context, state) => SizedBox(
+          width: state.mapReady ? double.infinity : 1,
+          height: state.mapReady ? double.infinity : 1,
+          child: GoogleMap(
+            mapType: MapType.normal,
+            markers: state.markers,
+            initialCameraPosition: state.initialPosition(),
+            onMapCreated: state.onMapCreated,
+          ),
         ),
       ),
     );
