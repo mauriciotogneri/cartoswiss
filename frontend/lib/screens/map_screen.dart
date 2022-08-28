10d1689d645a75ffddfbc3100dfc6ccd1e136f4c
Mauricio Togneri <mauricio.togneri@gmail.com>
2022-08-28 21:37:17 +0200

    Improvements

--- frontend/lib/screens/map_screen.dart
+++ frontend/lib/screens/map_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cartoswiss/services/palette.dart';
 import 'package:cartoswiss/states/map_state.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatelessWidget {
   final MapState state;
@@ -23,13 +24,17 @@ class Content extends StatelessWidget {
 
   const Content(this.state);
 
+  CameraPosition get _cameraPosition => const CameraPosition(
+        target: LatLng(46.194641, 6.143514),
+        zoom: 18,
+      );
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Carto Swiss',
-        style: TextStyle(color: Palette.white),
-      ),
+    return GoogleMap(
+      mapType: MapType.normal,
+      initialCameraPosition: _cameraPosition,
+      onMapCreated: state.controller.complete,
     );
   }
 }
--- frontend/lib/states/map_state.dart
+++ frontend/lib/states/map_state.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
 import 'package:dafluta/dafluta.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapState extends BaseState {}
+class MapState extends BaseState {
+  final Completer<GoogleMapController> controller = Completer();
+}
